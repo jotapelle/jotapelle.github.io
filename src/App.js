8b90abf052e9c8065c9d32a871b5a32a3271876e
John <john@Johns-MacBook-Air.local>
2023-02-18 14:40:59 +0100

    rebranding

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as Icon from "react-bootstrap-icons";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Hi, I'm Jordi!</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I'm a .Net developer with an emphasis on C#, .NET, MVC, and EF for web
+          applications. Take a look at my GitHub profile!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <a
+            className="App-link"
+            href="https://github.com/jotapelle?tab=repositories"
+            target="_blank"
+            rel="noreferrer"
+            title="Github"
+          >
+            <Icon.Github />
+          </a>
+          <a
+            className="App-link"
+            href="https://www.freecodecamp.org/jotapelle"
+            target="_blank"
+            rel="noreferrer"
+            title="Freecodecamp"
+          >
+            <Icon.CodeSquare />
+          </a>
+          <a
+            className="App-link"
+            href="https://codepen.io/jotapelle/"
+            target="_blank"
+            rel="noreferrer"
+            title="Codepen"
+          >
+            <Icon.BoxSeam />
+          </a>
+          <a
+            className="App-link"
+            href="https://www.linkedin.com/in/jordi-p-50240a112/"
+            target="_blank"
+            rel="noreferrer"
+            title="Linkedin"
+          >
+            <Icon.Linkedin />
+          </a>
+        </div>
       </header>
     </div>
   );
